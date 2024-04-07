27123200b322bf7c08fb2ba373ac1a84e0e9830e
SentryPrimies <sentryprime92@gmail.com>
2024-04-07 17:07:43 +0300

    финальный гейминг

--- Content.Server/Imperial/Telebaton/TelebatonComponent.cs
+++ Content.Server/Imperial/Telebaton/TelebatonComponent.cs
@@ -5,18 +5,16 @@ using Robust.Shared.Audio;
 namespace Content.Server.Imperial.Telebaton
 {
   [RegisterComponent, Access(typeof(TelebatonSystem))]
-  public sealed class TelebatonComponent : Component 
+  public sealed partial class TelebatonComponent : Component 
   {
     public bool Activated { get; set; } = false;
-
-    [ViewVariables(VVAccess.ReadWrite)]
     [DataField("onThrowStunChance")]
-    public float onThrowStunChance { get; set; } = 0.20f;
+    public float onThrowStunChance { get; set; } = 0.7f;
 
     [DataField("stunSound")]
-    public SoundSpecifier stunsound { get; set; } = new SoundPathSpecifier("/Audio/Weapons/Guns/Gunshots/bang.ogg");
+    public SoundSpecifier stunsound { get; set; } = new SoundPathSpecifier("/Audio/Imperial/Batonening/bonk-sound-effect.ogg");
 
     [DataField("extendSound")]
-    public SoundSpecifier extendsound { get; set; } = new SoundPathSpecifier("/Audio/Weapons/Guns/Gunshots/bang.ogg");
+    public SoundSpecifier extendsound { get; set; } = new SoundPathSpecifier("/Audio/Imperial/Batonening/batonextend.ogg");
   }
 }
--- Content.Server/Imperial/Telebaton/TelebatonSystem.cs
+++ Content.Server/Imperial/Telebaton/TelebatonSystem.cs
@@ -1,6 +1,7 @@
 using Content.Server.Stunnable.Components;
 using Content.Shared.Audio;
 using Content.Shared.Damage.Events;
+using Content.Shared.Damage.Components;
 using Content.Shared.Examine;
 using Content.Shared.Interaction.Events;
 using Content.Shared.Item;
@@ -14,86 +15,87 @@ using Robust.Shared.Player;
 
 namespace Content.Server.Imperial.Telebaton
 {
-  public sealed class TelebatonSystem : EntitySystem 
-  {
-    [Dependency] private readonly SharedItemSystem _item = default!;
-    [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
-    [Dependency] private readonly SharedAudioSystem _audio = default!;
-    public override void Initialize() 
+    public sealed partial class TelebatonSystem : EntitySystem
     {
-      base.Initialize();
+        [Dependency] private readonly SharedItemSystem _item = default!;
+        [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
+        [Dependency] private readonly SharedAudioSystem _audio = default!;
 
-      SubscribeLocalEvent<TelebatonComponent, UseInHandEvent>(OnUseInHand);
-      SubscribeLocalEvent<TelebatonComponent, ExaminedEvent>(OnExamined);
-      SubscribeLocalEvent<TelebatonComponent, StaminaDamageOnHitAttemptEvent>(OnStaminaHitAttempt);
-      SubscribeLocalEvent<TelebatonComponent, MeleeHitEvent>(OnMeleeHit);
-    }
+        public override void Initialize()
+        {
+            base.Initialize();
 
-    private void OnUseInHand(EntityUid uid, TelebatonComponent component, UseInHandEvent arguments) 
+            SubscribeLocalEvent<TelebatonComponent, UseInHandEvent>(OnUseInHand);
+            SubscribeLocalEvent<TelebatonComponent, ExaminedEvent>(OnExamined);
+            SubscribeLocalEvent<TelebatonComponent, StaminaDamageOnHitAttemptEvent>(OnStaminaHitAttempt);
+            SubscribeLocalEvent<TelebatonComponent, MeleeHitEvent>(OnMeleeHit);
+        }
+
+        private void OnUseInHand(EntityUid uid, TelebatonComponent component, UseInHandEvent arguments)
+        {
+            if (component.Activated)
+            {
+                RetractTelebaton(uid, component);
+            }
+            else
+            {
+                ExtendTelebaton(uid, component, arguments.User);
+            }
+        }
+
+        private void RetractTelebaton(EntityUid uid, TelebatonComponent component)
+        {
+            if (!component.Activated)
+                return;
+
+            if (TryComp<AppearanceComponent>(component.Owner, out var appearance) && TryComp<ItemComponent>(component.Owner, out _))
+            {
+                _item.SetHeldPrefix(component.Owner, "retracted");
+                _appearance.SetData(uid, ToggleVisuals.Toggled, false, appearance);
+            }
+
+            _audio.PlayPvs(component.extendsound, uid);
+            component.Activated = false; 
+        }
+
+        private void ExtendTelebaton(EntityUid uid, TelebatonComponent component, EntityUid user)
+        {
+            if (component.Activated)
+                return;
+
+            if (TryComp<AppearanceComponent>(component.Owner, out var appearance) && TryComp<ItemComponent>(component.Owner, out _))
+            {
+                _item.SetHeldPrefix(component.Owner, "extended");
+                _appearance.SetData(uid, ToggleVisuals.Toggled, true, appearance);
+            }
+
+            _audio.PlayPvs(component.extendsound, uid);
+            component.Activated = true;
+        }
+
+        private void OnExamined(EntityUid uid, TelebatonComponent component, ExaminedEvent args)
+        {
+            var msg = component.Activated
+                ? Loc.GetString("melee-telebaton-examined-extended")
+                : Loc.GetString("melee-telebaton-examined-retracted");
+            args.PushMarkup(msg);
+        }
+
+private void OnStaminaHitAttempt(EntityUid uid, TelebatonComponent component, ref StaminaDamageOnHitAttemptEvent args)
+{
+    if (!component.Activated)
     {
-      if (component.Activated) 
-      {
-        RetractTelebaton(uid, component);
-      }
-      else 
-      {
-        ExtendTelebaton(uid, component, arguments.User);
-      }
-    }
-
-    private void RetractTelebaton(EntityUid uid, TelebatonComponent component) 
-    {
-      if (!component.Activated)
-        return;
-
-      if (TryComp<AppearanceComponent>(component.Owner, out var appearance) && TryComp<ItemComponent>(component.Owner, out var item)) 
-      {
-        _item.SetHeldPrefix(component.Owner, "retracted", item);
-        _appearance.SetData(uid, ToggleVisuals.Toggled, false, appearance);
-      }
-
-      _audio.PlayPvs(component.stunSound);
-      component.Activated = false;
-    }
-
-    private void ExtendTelebaton(EntityUid uid, TelebatonComponent component, EntityUid user) 
-    {
-      if (component.Activated)
-        return;
-
-      if (EntityManager.TryGetComponent<AppearanceComponent>(component.Owner, out var appearance) && EntityManager.TryGetComponent<ItemComponent>(component.Owner, out var item)) 
-      {
-        _item.SetHeldPrefix(component.Owner, "extended", item);
-        _appearance.SetData(uid, ToggleVisuals.Toggled, true, appearance);
-      }
-
-        _audio.PlayPvs(component.extendSound);
-      component.Activated = true;
-    }
-
-    private void OnExamined(EntityUid uid, TelebatonComponent component, ExaminedEvent args) 
-    {
-      var msg = component.Activated
-        ? Loc.GetString("melee-telebaton-examined-extended")
-        : Loc.GetString("melee-telebaton-examined-retracted");
-      args.PushMarkup(msg);
-    }
-
-    private void OnStaminaHitAttempt(EntityUid uid, TelebatonComponent component, ref StaminaDamageOnHitAttemptEvent args) 
-    {
-      if (!component.Activated) 
-      {
         args.Cancelled = true;
         return;
-      }
-      args.HitSoundOverride = component.StunSound;
     }
+    //args.HitSoundOverride = component.stunsound;
+}
 
-    private void OnMeleeHit(EntityUid uid, TelebatonComponent component, MeleeHitEvent args) 
-    {
-      if (!component.Activated)
-        return;
-      args.BonusDamage -= args.BaseDamage;
+        private void OnMeleeHit(EntityUid uid, TelebatonComponent component, MeleeHitEvent args)
+        {
+            if (!component.Activated)
+                return;
+            args.BonusDamage -= args.BaseDamage;
+        }
     }
-  }
 }
